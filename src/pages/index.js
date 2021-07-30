e5b289002ec7db20ecdd1021843a609bed648fba
omobosteven <omobosteven@gmail.com>
2021-07-30 17:35:47 +0100

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,16 @@
 import * as React from "react";
+import { StaticImage } from "gatsby-plugin-image";
 import Layout from "../components/layout";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+        // src="../images/45640.jpg"
+      />
     </Layout>
   );
 };
